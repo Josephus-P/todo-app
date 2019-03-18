6b8631c6ba5ea059582183c7c3fa6d31c6accda9
Josue Peralta <jperal77@gmail.com>
2019-03-18 03:43:18 -0500

    fixed import issue

--- todos-router/todos-router.js
+++ todos-router/todos-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./db/dbConfig');
+const db = require('../db/dbConfig');
 
 // Return all todos for a user
 router.get('/users/todos', async (req, res) => {
