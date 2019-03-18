b023f8a22266a1db5670eb802c1d125b9238d0e0
Josue Peralta <jperal77@gmail.com>
2019-03-18 02:36:09 -0500

    Prepared the server for production deployment with Postgres

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -12,35 +12,16 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations',
+      directory: './db/migrations',
     },
+    useNullAsDefault: true,
   },
 };
