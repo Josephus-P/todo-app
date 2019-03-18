6f3c8648ae7fa011c1c57ac185239d65ea99a0f6
Josue Peralta <jperal77@gmail.com>
2019-03-18 02:25:40 -0500

    updated the homepage

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Loader from '../loader/Loader';
 import Typography from '@material-ui/core/Typography';
-
-const styles = theme => ({
-  loading: {
-    marginTop: '25%',
-    padding: theme.spacing.unit * 3,
-  },
-});
+import styles from './styles';
 
 const Home = props => {
   const { loading, authUser, classes } = props;
@@ -24,20 +18,32 @@ const Home = props => {
   }
 
   return (
-    <>
-      <Typography component="h1" variant="h1">
+    <Paper className={classes.paper}>
+      <Typography
+        className={classes.text}
+        component="h1"
+        variant="h1"
+        align="center"
+      >
         Yaro Todo
       </Typography>
-      <Typography component="p" variant="h5">
+      <Typography
+        className={classes.text}
+        component="p"
+        variant="h5"
+        align="center"
+      >
         The only todo app you need!
       </Typography>
-      <Button variant="contained" component={Link} to="/login">
-        Login
-      </Button>
-      <Button variant="contained" component={Link} to="/register">
-        Signup
-      </Button>
-    </>
+      <div className={classes.buttons}>
+        <Button variant="contained" component={Link} to="/login">
+          Login
+        </Button>
+        <Button variant="contained" component={Link} to="/register">
+          Signup
+        </Button>
+      </div>
+    </Paper>
   );
 };
 
---
+++ client/src/components/home/styles.js
@@ -0,0 +1,23 @@
+const styles = theme => ({
+  loading: {
+    marginTop: '25%',
+    padding: theme.spacing.unit * 3,
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    width: '50%',
+    margin: 'auto',
+  },
+  paper: {
+    width: '50%',
+    margin: 'auto',
+    padding: theme.spacing.unit * 3,
+    marginTop: '100px',
+  },
+  text: {
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+export default styles;
