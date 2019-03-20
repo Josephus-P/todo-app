c4743ee8d6965c16a62128e194e2dd3aabdc07d5
Josue Peralta <jperal77@gmail.com>
2019-03-20 15:25:49 -0500

    added db seeds

---
+++ db/seeds/todos.js
@@ -0,0 +1,31 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('todos')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('todos').insert([
+        {
+          id: 1,
+          title: 'My first todo',
+          user_uid: 'rowValue1',
+          createdAt: '3/19/19',
+          description: 'This is my first todo',
+        },
+        {
+          id: 2,
+          title: 'My second todo',
+          user_uid: 'rowValue2',
+          createdAt: '3/20/19',
+          description: 'This is my second todo',
+        },
+        {
+          id: 3,
+          title: 'My third todo',
+          user_uid: 'rowValue3',
+          createdAt: '3/21/19',
+          description: 'This is my third todo',
+        },
+      ]);
+    });
+};
---
+++ db/seeds/users.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('users').insert([
+        { id: 1, uid: 'rowValue1' },
+        { id: 2, uid: 'rowValue2' },
+        { id: 3, uid: 'rowValue3' },
+      ]);
+    });
+};
