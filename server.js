317edf5a54f3c5300e3e64cc7b244ec9198605e1
Josue Peralta <jperal77@gmail.com>
2019-03-20 16:23:20 -0500

    moved firebase admin setup to separate folder

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 const express = require('express');
 const server = express();
 const cors = require('cors');
-const admin = require('firebase-admin');
+const admin = require('./firebase-admin/admin');
 const db = require('./db/dbConfig');
 const todosRouter = require('./todos-router/todos-router.js');
 require('dotenv').load();
 
 server.use(express.json());
 server.use(cors());
-
-admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url:
-      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  }),
-});
 
 // Middleware function to verify Firebase token
 // Adds the user's UID to the request body
