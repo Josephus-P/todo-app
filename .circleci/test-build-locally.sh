ba66ff38201a19db0f2918748afb4291b6a7a9fd
Josue Peralta <jperal77@gmail.com>
2019-03-25 19:54:01 -0500

    circleci config update

--- .circleci/test-build-locally.sh
+++ .circleci/test-build-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=9fda9cd7a7932058d797d516fdce15799cda5c91\
+    --form revision=3cbb8b94f45c3ad612929817f89150e0aab2b75f\
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/Josephus-P/yaro-todo/tree/master
